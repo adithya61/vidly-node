ba3a7157a2fd85a684c64732d18d6b7a060cfeb3
adithya61 <zonic662@gmail.com>
2023-07-24 19:08:11 +0530

    db

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function() {
-  const db = config.get('db');
+  const db = process.env.vidly_db;
   mongoose.connect(db)
     .then(() => winston.info(`Connected to ${db}...`));
 }
