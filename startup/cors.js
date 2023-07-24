c9d97afe46837caa58aa2a83bafdc7600f46a6e0
adithya61 <zonic662@gmail.com>
2023-07-24 16:35:46 +0530

    dddd

--- startup/cors.js
+++ startup/cors.js
@@ -1,6 +1,5 @@
 const cors = require("cors");
-const router = require("../routes/genres");
 
 module.exports = function (app) {
-  app.use("../dist/", router);
+  app.use(cors());
 };
--- startup/routes.js
+++ startup/routes.js
@@ -10,7 +10,7 @@ const error = require("../middleware/error");
 
 module.exports = function (app) {
   app.use(express.json());
-  app.use("/api/genres", genres);
+  app.use("../dist/api/genres", genres);
   app.use("/api/customers", customers);
   app.use("/api/movies", movies);
   app.use("/api/rentals", rentals);
@@ -18,5 +18,4 @@ module.exports = function (app) {
   app.use("/api/auth", auth);
   app.use("/api/returns", returns);
   app.use(error);
-
 };
