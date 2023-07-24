a70675c3cb15c4d289200d41296df1eb3c52457a
adithya61 <zonic662@gmail.com>
2023-07-24 16:10:39 +0530

    zz

--- dist/index.js
+++ dist/index.js
@@ -10,14 +10,19 @@ require("../startup/db")();
 // require("./startup/config")();
 require("../startup/validation")();
 
-
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
+
+app.get("/test", (req, res) => {
+  res.status(200).send({
+    message: "Hello World",
+  });
+});
+
 console.log("INFO: server set up done");
 
-app.use("/api/genres", genres);
 // "db": "mongodb+srv://zonic:syagAXVEthNGhAlx@test.snk5gii.mongodb.net/test?retryWrites=true&w=majority",
 // "port": "27017"
 module.exports = server;
