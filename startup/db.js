ce23d08fe93a48048a08fb278204329c916171e0
adithya61 <zonic662@gmail.com>
2023-07-24 19:11:31 +0530

    db

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,10 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-const config = require('config');
+const winston = require("winston");
+const mongoose = require("mongoose");
+const config = require("config");
 
-module.exports = function() {
-//   const db = process.env.vidly_db;
-  mongoose.connect('mongodb+srv://zonic:syagAXVEthNGhAlx@test.snk5gii.mongodb.net/test?retryWrites=true&w=majority')
-    .then(() => winston.info(`Connected to ${db}...`));
-}
+module.exports = function () {
+  //   const db = process.env.vidly_db;
+  const db =
+    "mongodb+srv://zonic:syagAXVEthNGhAlx@test.snk5gii.mongodb.net/test?retryWrites=true&w=majority";
+  mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
+};
