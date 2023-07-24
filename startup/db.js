25047604f507ea5d46bbe2fa37cdbc043bf85eea
adithya61 <zonic662@gmail.com>
2023-07-24 13:51:31 +0530

    db

--- startup/db.js
+++ startup/db.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = function () {
-  const db = config.get(
-    "mongodb+srv://zonic:syagAXVEthNGhAlx@test.snk5gii.mongodb.net/test?retryWrites=true&w=majority"
-  );
+  var mongo =
+    "mongodb+srv://zonic:syagAXVEthNGhAlx@test.snk5gii.mongodb.net/test?retryWrites=true&w=majority";
+  const db = config.get(mongo);
   mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
 };
